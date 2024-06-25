3c88ba8aa9a3aebc6cd3daf6c21f73e4117d9845
supakarn-t <hikaru8889@gmail.com>
2024-06-25 10:34:29 +0700

    userRoute

--- 01A-init-route/src/app.js
+++ 01A-init-route/src/app.js
@@ -1,8 +1,9 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 
 // ###### TASK : Import Route Here
-import authRoute from './routes/authRoute.js';
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -15,8 +16,9 @@ const PORT = process.env.PORT || 8000;
 // > URL Encoded Body Parser (รอเฉลย)
 
 // ##### TASK : Set Up Routes
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
--- 01A-init-route/src/routes/userRoute.js
+++ 01A-init-route/src/routes/userRoute.js
@@ -1,11 +1,20 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
 // API - 1 Get All Users
+router.get("/", (req, res) => {
+	res.send("GET /api/users");
+});
 
 // API - 2 Get User By ID
+router.get("/:id", (req, res) => {
+	res.send("GET /api/users/:id");
+});
 
 // API - 14 Delete User By ID (Soft Delete)
+router.delete("/:id", (req, res) => {
+	res.send("DELETE /api/users/:id");
+});
 
 export default router;

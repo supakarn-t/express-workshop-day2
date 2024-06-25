d8a91ec3d6d2c0a09e66dbee298d2c6a68495b07
supakarn-t <hikaru8889@gmail.com>
2024-06-25 11:15:06 +0700

    02A - movieListRoute

--- 02A-init-controller/src/routes/movieListRoute.js
+++ 02A-init-controller/src/routes/movieListRoute.js
@@ -1,40 +1,33 @@
-import express from 'express';
+import express from "express";
+import * as movieListController from "../controllers/movieListController.js";
 
 const router = express.Router();
 
 // API - 5 Get All Movie List
-router.get('/', (req, res) => {
-  res.send('GET /api/movie-lists');
-});
+router.get("/", movieListController.getAllMovieLists);
 
 // API - 6 Get Movie List By ID
-router.get('/:id', (req, res) => {
-  res.send('GET /api/movie-lists/:id');
-});
+router.get("/:id", movieListController.getMovieListById);
 
 // API - 7 Create Movie List
-router.post('/', (req, res) => {
-  res.send('POST /api/movie-lists');
-});
+router.post("/", movieListController.createMovieList);
 
-// API - 8 Update Movie List
-router.patch('/:id', (req, res) => {
-  res.send('PATCH /api/movie-lists/:id');
-});
+// API - 18 Update Movie List
+router.patch("/:id", movieListController.updateMovieList);
 
 // API - 17 Delete Movie List (Soft Delete)
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /api/movie-lists/:id');
-});
+router.delete("/:id", movieListController.deleteMovieList);
 
 // API - 8  Add movie to movie list
-router.post('/:movieListId/movies/:movieId', (req, res) => {
-  res.send('POST /api/movie-lists/:movieListId/movies/:movieId');
-});
+router.post(
+	"/:movieListId/movies/:movieId",
+	movieListController.addMovieToMovieList
+);
 
 // API - 16  Remove movie from movie list
-router.delete('/:movieListId/movies/:movieId', (req, res) => {
-  res.send('DELETE /api/movie-lists/:movieListId/movies/:movieId');
-});
+router.delete(
+	"/:movieListId/movies/:movieId",
+	movieListController.removeMovieFromMovieList
+);
 
 export default router;
